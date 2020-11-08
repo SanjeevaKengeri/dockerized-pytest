cd28ba43c84ff8870ee5d32719c97b711a9c43d8
Sanjeev <sanjeev.kengeri@gmail.com>
2020-11-08 18:31:49 +0000

    first test case and module using TDD

--- scripts/chp2/video2/mapmaker_start.py
+++ scripts/chp2/video2/mapmaker_start.py
@@ -1,2 +1,11 @@
 class Point():
-    pass
+    def __init__(self,name,latitude,longitude):
+        self._name = name
+        self._latitude = latitude
+        self._longitude = longitude
+    #if we want to make sure that these instance objects 
+    #are not accessible to the average user we should 
+    #put a _ in front of each one.
+    
+    def get_lat_long(self):
+        return (self._latitude,self._longitude)
--- tests/chp2/video2/test_mapmaker_start.py
+++ tests/chp2/video2/test_mapmaker_start.py
@@ -1,5 +1,6 @@
+from scripts.chp2.video2.mapmaker_start import Point
 
 
 def test_make_one_point():
-    
-    pass
+    P1 = Point("Dakar",17.4564, 18.6789)
+    assert P1.get_lat_long() == ((17.4564, 18.6789))
