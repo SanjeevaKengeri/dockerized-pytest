dfa85346244161bb10d4e80fa06b12ed5303ff9c
Sanjeev <sanjeev.kengeri@gmail.com>
2020-11-08 19:21:30 +0000

    fix to first test case

--- tests/chp2/video2/test_mapmaker_start.py
+++ tests/chp2/video2/test_mapmaker_start.py
@@ -2,5 +2,5 @@ from scripts.chp2.video2.mapmaker_start import Point
 
 
 def test_make_one_point():
-    P1 = Point("Dakar",17.4564, 18.6789)
+    P1 = Point("Dakar", 17.4564, 18.6789)
     assert P1.get_lat_long() == ((17.4564, 18.6789))
